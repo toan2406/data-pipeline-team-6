eb4e5ce0f3e8978654f71f409cde6bd9f0739f93
Toan Nguyen <toan2406@gmail.com>
2016-11-02 18:49:34 +0700

    collector reponses the json body

--- log-collector/app.js
+++ log-collector/app.js
@@ -2,17 +2,18 @@ var fs = require('fs')
 var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
+var kafka = require('kafka-node')
 
 app.use(bodyParser.json())
 
 app.post('/api/events', function (req, res) {
-  fs.appendFile('./logs', JSON.stringify(req.body) + '\r\n', function (err) {
-    if (!err) {
-      res.status(200).send('OK')
-    } else {
-      res.status(500).send('Fail')
-    }
-  })
+  // fs.appendFile('./logs', JSON.stringify(req.body) + '\r\n', function (err) {
+  //   if (!err) {
+  res.status(200).send(req.body)
+  //   } else {
+  //     res.status(500).send('Fail')
+  //   }
+  // })
 })
 
 app.listen(3000, function () {
