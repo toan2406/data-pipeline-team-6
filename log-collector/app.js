396bf0decf29b0ff2f83215f837bb4d02356e551
Toan Nguyen <toan2406@gmail.com>
2016-11-04 21:10:36 +0700

    connect producer to zoo1

--- log-collector/app.js
+++ log-collector/app.js
@@ -1,21 +1,39 @@
-var fs = require('fs')
+
 var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
 var kafka = require('kafka-node')
+var Producer = kafka.Producer
+var client = new kafka.Client('zoo1:2181')
+var producer = new Producer(client)
 
-app.use(bodyParser.json())
+producer.on('ready', function () {
+  app.use(bodyParser.json())
 
-app.post('/api/events', function (req, res) {
-  // fs.appendFile('./logs', JSON.stringify(req.body) + '\r\n', function (err) {
-  //   if (!err) {
-  res.status(200).send(req.body)
-  //   } else {
-  //     res.status(500).send('Fail')
-  //   }
-  // })
+  app.post('/api/events', function (req, res) {
+    var payloads = [
+      {
+        topic: 'events',
+        messages: JSON.stringify(req.body),
+        partition: 0
+      }
+    ]
+
+    producer.send(payloads, function (err, data) {
+      if (err) {
+        res.status(500).send(err)
+      } else {
+        res.status(200).send(err)
+      }
+    })
+  })
+
+  app.listen(3000, function () {
+    console.log('Log collector is ready!')
+  })
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+producer.on('error', function (err) {
+  console.log('Error:')
+  console.log(err)
 })
